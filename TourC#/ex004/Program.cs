d058ce272049c085d303fd0f6d93942daeee15ac
Aleksandr D <bestacc@mail.ru>
2022-08-07 20:21:05 +0300

    Added The greater of the three numbers program

---
+++ TourC#/ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine(max);

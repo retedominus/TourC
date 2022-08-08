3295655d70a783d01e5394d0eaffaae5a5726395
Aleksandr D <bestacc@mail.ru>
2022-08-08 13:15:51 +0300

    The greater number program edit

--- TourC#/ex002/Program.cs
+++ TourC#/ex002/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Программа: Какое число большее, а какое меньшее
 //В описании указано, что нужно вывести большее и меньшее число
 //Сдалал как было в приведенном примере
-Console.WriteLine("Введите число a: ");
+Console.Write("Введите число a: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число b: ");
+Console.Write("Введите число b: ");
 int b = Convert.ToInt32(Console.ReadLine());
 int max = a;
 if (b > max) max = b;

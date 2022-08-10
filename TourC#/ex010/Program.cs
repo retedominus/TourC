577414e1ccf13c1a45ec8820f0e4689fb3513dbe
Aleksandr D <bestacc@mail.ru>
2022-08-10 15:56:07 +0300

    modified sheme

--- TourC#/ex010/Program.cs
+++ TourC#/ex010/Program.cs
@@ -4,8 +4,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 if ((number / 100 == 0) | (number / 1000 != 0))
 {
     Console.WriteLine("ВВЕДЕНО НЕВЕРНОЕ ЗНАЧЕНИЕ");
+
 }
-else 
+else
 {
     int secondNumber = (number % 100) / 10;
     Console.WriteLine(secondNumber);

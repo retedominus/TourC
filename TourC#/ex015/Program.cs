cdb8ca5381f85651cafea41123cc3dfa6c63d942
Aleksandr D <bestacc@mail.ru>
2022-08-10 19:22:13 +0300

    Added weekend program

---
+++ TourC#/ex015/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите порядковый номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 7)
+{
+    Console.WriteLine("нет такого дня недели");
+}
+else
+{
+    if ((num == 6) | (num == 7))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}

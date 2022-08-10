38d83faf8ca446d7ceed3f4d193146eef224a73a
Aleksandr D <bestacc@mail.ru>
2022-08-10 13:35:20 +0300

    Modified ex010

--- TourC#/ex010/Program.cs
+++ TourC#/ex010/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите трехзначное число: ");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int secondNumber = (number % 100) / 10;
-Console.WriteLine(secondNumber);
+if ((number / 100 == 0) | (number / 1000 != 0))
+{
+    Console.WriteLine("ВВЕДЕНО НЕВЕРНОЕ ЗНАЧЕНИЕ");
+}
+else 
+{
+    int secondNumber = (number % 100) / 10;
+    Console.WriteLine(secondNumber);
+}

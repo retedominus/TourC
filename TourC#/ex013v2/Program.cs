c0774759fbad0e422ee36ba654dc3d9f4c3668b1
Aleksandr D <bestacc@mail.ru>
2022-08-11 17:02:47 +0300

    Added 2nd version Third number program

---
+++ TourC#/ex013v2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length <= 2)
+{
+     Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(number[2]);
+}

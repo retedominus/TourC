2fa34d7b7293c08391c00442705ce15e7e84c3a9
Aleksandr D <bestacc@mail.ru>
2022-08-17 13:13:54 +0300

    Added Cube table print program

---
+++ TourC#/ex023/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+int[] CubeTab(int arg)
+{
+    int num = 0;
+    int[] array = new int[arg];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = (num + 1) * (num + 1) * (num + 1);
+        num++;
+    }
+
+    return array;
+}
+
+System.Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] Cub = CubeTab(N);
+System.Console.Write($"{N} -> ");
+PrintArray(Cub);

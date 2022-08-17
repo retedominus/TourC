c87418ebf30c9cb04301004c39ea613e9fdc7e25
Aleksandr D <bestacc@mail.ru>
2022-08-17 13:14:24 +0300

    edit Library

--- TourC#/Library.cs
+++ TourC#/Library.cs
@@ -1,102 +1,152 @@
-//Метод расчета расстояния между двумя точками
-double Distance(int[] A, int [] B)
+public static class Library
 {
-    int[] temp = new int[A.Length];
-    double dist = 0;
-    int sum = 0;
-    if (A.Length != B.Length) System.Console.WriteLine("Введены неверные координаты одной из точек");
-    else
+    //Метод расчета расстояния между двумя точками
+    public static double Distance(int[] A, int[] B)
     {
-        for (int i = 0; i < A.Length; i++)
+        int[] temp = new int[A.Length];
+        double dist = 0;
+        int sum = 0;
+        if (A.Length != B.Length) System.Console.WriteLine("Введены неверные координаты одной из точек");
+        else
         {
-            temp[i] = B[i] - A[i];
+            for (int i = 0; i < A.Length; i++)
+            {
+                temp[i] = B[i] - A[i];
 
+            }
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i] = temp[i] * temp[i];
+
+                sum = sum + temp[i];
+            }
         }
+        return dist = Math.Sqrt(Convert.ToDouble(sum));
+    }
+
+    //Метод конвертации строки в массив
+    public static int[] StringToArray(string str)
+    {
+        string[] temp = str.Split(",");
+        int[] array = new int[temp.Length];
         for (int i = 0; i < temp.Length; i++)
         {
-            temp[i] = temp[i] * temp[i];
-
-            sum = sum + temp[i];
+            array[i] = Convert.ToInt32(temp[i]);
         }
+        return array;
     }
-return dist = Math.Sqrt(Convert.ToDouble(sum));
-}
 
-//Метод конвертации строки в массив
-int[] StringToArray(string str)
-{
-    string[] temp = str.Split(",");
-    int[] array = new int[temp.Length];
-    for (int i = 0; i < temp.Length; i++)
+    //Метод подсчета количества цифр в числе
+    public static int NumOfDig(int arg)
     {
-        array[i] = Convert.ToInt32(temp[i]);
+        int div = 10;
+        int count = 1;
+        int value = arg / 10;
+        while (value != 0)
+        {
+            value = arg / div;
+            div = div * 10;
+            if (value != 0) count++;
+        }
+        return count;
     }
-    return array;
-}
 
-//Метод подсчета количества цифр в числе
-int NumOfDig(int arg)
-{
-    int div = 10;
-    int count = 1;
-    int value = arg / 10;
-    while (value != 0)
+    //Метод конвертации числа в массив
+    public static int[] ConvertToArray(int arg)
     {
-        value = arg / div;
-        div = div * 10;
-        if (value != 0) count++;
+        int div = 10;
+        int count = 0;
+        int number = arg / div;
+        int digit = arg % div;
+        int[] array = new int[6];
+        int[] temp = new int[array.Length];
+        int j = temp.Length;
+        while (count < temp.Length)
+        {
+            temp[count] = digit;
+            digit = number % div;
+            number = number / div;
+            count++;
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = temp[j - 1];
+            j--;
+        }
+        return array;
     }
-    return count;
-}
 
-//Метод конвертации числа в массив
-int[] ConvertToArray(int arg)
-{
-    int div = 10;
-    int count = 0;
-    int number = arg / div;
-    int digit = arg % div;
-    int[] array = new int[6];
-    int[] temp = new int[array.Length];
-    int j = temp.Length;
-    while (count < temp.Length)
+    //Метод проверки числа на палиндром
+    public static void PalCheck(int[] array)
     {
-        temp[count] = digit;
-        digit = number % div;
-        number = number / div;
-        count++;
+        int j = array.Length - 1;
+        int x = 0;
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            if (array[i] == array[j]) x++;
+            j--;
+        }
+        if (x == array.Length / 2) System.Console.WriteLine("да");
+        else System.Console.WriteLine("нет");
     }
-    for (int i = 0; i < array.Length; i++)
+
+    //Метод вывода в терминал массива
+    public static void PrintArray(int[] array)
     {
-        array[i] = temp[j - 1];
-        j--;
+        for (int i = 0; i < array.Length; i++)
+        {
+            System.Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
     }
-    return array;
-}
 
-//Метод проверки числа на палиндром
-void PalCheck(int[] array)
-{
-    int j = array.Length - 1;
-    int x = 0;
-    for (int i = 0; i < array.Length / 2; i++)
+    //Метод сложения матриц
+    public static int[] SumMatrix(int[] m1, int[] m2)
     {
-        if (array[i] == array[j]) x++;
-        j--;
+        int[] sum = new int[m1.Length];
+        for (int j = 0; j < m1.Length; j++)
+        {
+            sum[j] = m1[j] + m2[j];
+        }
+        return sum;
     }
-    if (x == array.Length / 2) System.Console.WriteLine("да");
-    else System.Console.WriteLine("нет");
-}
 
-//Метод вывода в терминал массива
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
+    ////Метод 
+    public static string res(int[] x)
     {
-        System.Console.Write($"{array[i]} ");
+        return string.Join(", ", x);
     }
-    Console.WriteLine();
+
+    //Метод сравнения длинны массивов
+    public static bool check(int[] x, int[] y)
+    {
+        return x.Length == y.Length;
+    }
+
+    //Замена слов в тексте
+    public static string Replace(string text, char oldValue, char newValue)
+    {
+        string result = String.Empty;
+        int length = text.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (text[i] == oldValue) result = result + $"{newValue}";
+            else result = result + $"{text[i]}";
+        }
+        return result;
+    }
+
+    //Метод вывода кубов чисел от 1 до N
+    public static int[] CubeTab(int arg)
+    {
+        int num = 0;
+        int[] array = new int[arg];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = (num + 1) * (num + 1) * (num + 1);
+            num++;
+        }
+
+        return array;
+    }
 }
-
-
-

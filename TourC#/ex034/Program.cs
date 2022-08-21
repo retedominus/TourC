41e67cd33e89d17ca41960cd5cc5444c1bae0871
Aleksandr D <bestacc@mail.ru>
2022-08-21 21:34:52 +0300

    Added ex34 solution

---
+++ TourC#/ex034/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] FillArrayRandom(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArrayEx(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}]");
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    Console.WriteLine($" -> {count}");
+}
+
+System.Console.WriteLine("Введите длинну массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayRandom(N);
+PrintArrayEx(array);

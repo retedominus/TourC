88014c5d04ec2aeee28fbc78878433626a69d47c
Aleksandr D <bestacc@mail.ru>
2022-08-31 13:32:17 +0300

    edit Library

--- TourC#/ex047/Library.cs
+++ TourC#/ex047/Library.cs
@@ -222,5 +222,18 @@ public static class Library
         return array;
     }
 
+    //Метод заполнения двумерной матрицы случайными вещественными числами
+    public static double[,] CreateMatrix(int m, int n)
+    {
+        double[,] matrix = new double[m, n];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = Math.Round(new Random().Next(-10, 10) + new Random().NextDouble(), 2);
+            }
+        }
+        return matrix;
 
+    }
 }

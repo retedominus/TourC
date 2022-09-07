a92e6107ae9a37c346ccff3a556433e77aedfebd
Aleksandr D <bestacc@mail.ru>
2022-09-07 23:03:14 +0300

    edit Library

--- TourC#/Library.cs
+++ TourC#/Library.cs
@@ -322,5 +322,13 @@ public static class Library
         return resMtx;
     }
 
+    //Метод расчета функции Аккермана
+    public static int Ackerman(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        else if ((m > 0) && (n == 0)) return Ackerman(m - 1, 1);
+        else if ((m > 0) && (n > 0)) return Ackerman(m - 1, Ackerman(m, n - 1));
+        else return n + 1;
+    }
 
 }

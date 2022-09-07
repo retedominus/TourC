a50958fce32454d289070941d3ae1e646e9914e2
Aleksandr D <bestacc@mail.ru>
2022-09-07 23:02:41 +0300

    create Ackerman func

---
+++ TourC#/ex068/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n. m = 2, n = 3 -> A(m,n) = 9 (в условиях задачи ошибка не 29)
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m > 0) && (n == 0)) return Ackerman(m - 1, 1);
+    else if ((m > 0) && (n > 0)) return Ackerman(m - 1, Ackerman(m, n - 1));
+    else return n + 1;
+}
+
+int m = 2;
+int n = 3;
+System.Console.WriteLine($"m = {m}, n = {n} -> A(m, n) = {Ackerman(m, n)}");

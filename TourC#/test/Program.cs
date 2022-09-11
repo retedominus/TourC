4772824daae27d52eb29ea31005a0ca5d8374431
a.dergachev <bestacc@mail.ru>
2022-09-11 13:57:42 +0300

    added test folder

---
+++ TourC#/test/Program.cs
@@ -0,0 +1,22 @@
+﻿//метод проверки вводимого значения
+int check_int_input(string message)
+{
+    bool input_data_not_ok = true; //введены некорректные данные
+    int input = 0;                 //число для ввода
+    string err_message = "";
+    while (input_data_not_ok) //пока введены некорректные данные
+    {
+        Console.WriteLine($"{err_message}{message} :");
+        try
+        {
+            input = Convert.ToInt32(Console.ReadLine());
+            input_data_not_ok = false; //считаем что данные введены корректно
+        }
+        catch (SystemException)
+        {
+            input_data_not_ok = true; //пока введены некорректные данные
+            err_message = "Неправильный ввод. ";
+        }
+    }
+    return input;
+}
